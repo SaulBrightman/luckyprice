373599ee80d33ca81d974ce2e86db5dbd8701695
rich wilk <richardwilkinson@live.com>
2020-11-11 22:53:57 -0500

    api calls for specific data
    got some data to display on page needs styling

--- javascript.js
+++ javascript.js
@@ -6,17 +6,32 @@ $(document).ready(function() {
     today(user);
     
 });
-
-
-// 
-function today(user){
+$('#list').click(function(){
+    $.ajax({
+        method:"GET",
+        url: "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=75"
+    }).then(function(data) {
+        console.log(data);
+    })
+});
+today = (user) => {
     $.ajax({
         method: "GET",
         url: "https://www.cheapshark.com/api/1.0/deals?title="+user
-    
-    }).then ( function(data) {
+    }).then(function(data) {
         console.log(data);
-        $("#today").empty();
+        $("#display").empty();
+        let gameTitle = data[1].title;
+        let pOne = $('<p>').text("Title : " + gameTitle);
+        $("#display").append(pOne);
+        $("#price").empty();
+        let gamePrice = data[1].salePrice;
+        let pTwo = $('<p>').text("Price : " + gamePrice);
+        $("#price").append(pTwo);
+        $("#image").empty();
+        let gameImage = data[1].thumb;
+        let pThree = $('<img>').attr('src', gameImage);
+        $('#image').append(pThree);
     });
 
 
